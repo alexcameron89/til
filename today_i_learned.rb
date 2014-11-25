19c28dceb23765143031ef48a64565218d005268
Alex Kitchens <alexcameron98@gmail.com>
2014-11-25 16:09:25 -0600

    Fixes relative locations of the text file & Adds the expected look of this tool in a text file.

--- today_i_learned.rb
+++ today_i_learned.rb
@@ -6,37 +6,38 @@ line_number = ARGV.shift
 
 case argument
 when "add"
-	file = File.open('~/what_i_learned.txt','a')
-	file.write("#{Time.now.strftime('%m/%d/%y %k:%M%l.%S')}  -  #{message}\n")
+	file = File.open(ENV['HOME']+'/what_i_learned.txt','a')
+	file.write("#{Time.now.strftime('%m/%d/%y %k:%M%l.%S')}||#{message}\n")
 	puts "Added your message."
 when "show"
-	file = File.open('~/what_i_learned.txt','r')
+	file = File.open(ENV['HOME']+'/what_i_learned.txt','r')
 	counter = 0
 	file.readlines.each do |l|
 		puts "ID: #{counter}  -  #{l}"
 		counter += 1
 	end
 when 'edit'
-	file = File.open('~/what_i_learned.txt','r')
-	file_new = File.open('~/what_i_learned.txt.new','w')
+	file = File.open(ENV['HOME']+'/what_i_learned.txt','r')
+	file_new = File.open(ENV['HOME']+'/what_i_learned.txt.new','w')
 	counter = 0
 	file.readlines.each do |l|
 		if counter != line_number.to_i
 			file_new.write(l)
 		else
-			file_new.write("#{Time.now.strftime('%m/%d/%y - %k:%M%l.%S')} - #{message}\n")
+			file_new.write("#{Time.now.strftime('%m/%d/%y - %k:%M%l.%S')}||#{message}\n")
 			puts 'Message edited'
 		end
 		counter +=1
 	end
-	`mv what_i_learned.txt.new what_i_learned.txt`
+	`mv ~/what_i_learned.txt.new ~/what_i_learned.txt`
 when 'delete'
-  file = File.open('~/what_i_learned.txt','r')
-  file_new = File.open('~/what_i_learned.txt.new','w')
+  file = File.open(ENV['HOME']+'/what_i_learned.txt','r')
+  file_new = File.open(ENV['HOME']+'/what_i_learned.txt.new','w')
   counter = 0
   file.readlines.each do |l|
+		date, message = l.split(/||/)
     file_new.write(l) if counter != message.to_i
     counter +=1
   end
-  `mv what_i_learned.txt.new what_i_learned.txt`
+  `mv ~/what_i_learned.txt.new ~/what_i_learned.txt`
 end
